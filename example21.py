d4cf380098825f79186c4b6d2da78603262ee40f
emadam <ldy9037@naver.com>
2022-04-16 22:16:26 +0900

    예제 21번 작성

---
+++ example21.py
@@ -0,0 +1,11 @@
+def simple_interest(p, r, t):
+    return p * r * t
+
+def simple_interest_amount(p, r, t):
+    return p * (1 + r * t)
+
+print(simple_interest(10000000, 0.03875, 5))
+print(simple_interest(1100000, 0.05, 5/12))
+
+print(simple_interest_amount(10000000, 0.03875, 5))
+print(simple_interest_amount(1100000, 0.05, 5/12))
