b66128499031a67d1a40a36057434992fc263c73
emadam <ldy9037@naver.com>
2022-04-21 12:18:00 +0900

    다른 사람의 풀이 메모 추가

--- algorithm-lv1/q5.py
+++ algorithm-lv1/q5.py
@@ -12,7 +12,7 @@ def solution(board, moves):
         
             if not selected == 0:
                 board[depth][move - 1] = 0
-                
+
                 if  len(doll_stack) > 0 and doll_stack[-1] == selected: 
                     doll_stack.pop()
                     answer += 2
@@ -26,3 +26,22 @@ board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
 moves = [1,5,3,5,1,2,1,4]
 
 print(solution(board, moves))
+
+"""
+행과 열을 바꾼 후 0을 제거해서 처리하려다가 코드가 너무 지저분해져서 위 처럼 했었음. 그런데 다른 사람의 풀이를 보니 딱 원하던 모양새의 풀이가 있어서 적어둠. 
+
+def solution(board, moves):
+    cols = list(map(lambda x: list( filter(lambda y: y > 0, x) ), zip(*board)))
+
+    print(cols)
+    a, s = 0, [0]
+
+    for m in moves:
+        if len(cols[m - 1]) > 0:
+            if (d := cols[m - 1].pop(0)) == (l := s.pop()):
+                a += 2
+            else:
+                s.extend([l, d])
+
+    return a
+"""
