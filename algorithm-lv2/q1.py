9be01d2658f712ef6c6777fbac1785e2e66c32fc
emadam <ldy9037@naver.com>
2022-04-25 13:02:06 +0900

    해시 - 전화번호 목록

--- algorithm-lv2/q1.py
+++ algorithm-lv2/q1.py
@@ -1,15 +1,32 @@
-# 코딩테스트 연습 - 전화번호 목록
+# 해시 - 전화번호 목록
+# 성능 개선 필요함. 
+"""
 def solution(phone_book):
     phone_book.sort(key=len)
     phone_set = set()
+    phone_len = set()
 
     for phone in phone_book:
-        for prev_phone in phone_set: 
-            if phone.startswith(prev_phone): return False
-        
+        for length in phone_len: 
+            if phone[:length] in phone_set: return False
+                    
+        phone_len.add(len(phone))
         phone_set.add(phone)
 
     return True
+"""
+def solution(phoneBook):
+    #phoneBook = sorted(phoneBook)
+    phoneBook.sort()
+    print("정렬",phoneBook)
 
-phone_book = ["119","1195524421", "97674223"]
+    for p1, p2 in zip(phoneBook, phoneBook[1:]):
+        print('첫 번째', p1)
+        print('두 번째', p2)
+        if p2.startswith(p1):
+            return False
+    return True
+
+
+phone_book = ["119","97674223","1195524421"]
 print(solution(phone_book))
