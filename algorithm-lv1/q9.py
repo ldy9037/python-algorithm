e1c7779cf65bca5d0ef1e25d0d3399e3d5df5807
emadam <ldy9037@naver.com>
2022-04-25 13:02:54 +0900

    해시 - 전화번호 목록

--- algorithm-lv1/q9.py
+++
@@ -1,8 +0,0 @@
-# 코딩테스트 연습 - 전화번호 목록
-def solution(phone_book):
-    answer = True
-    
-    return answer
-
-phone_book = ["119", "97674223", "1195524421"]
-print(solution)
--- algorithm-lv2/q1.py
+++ algorithm-lv2/q1.py
@@ -1,6 +1,9 @@
 # 해시 - 전화번호 목록
 # 성능 개선 필요함. 
 """
+문자열 길이 순으로 정렬해서 비교하고 있는데 그냥 문자열 순으로 정렬해서 인접한 것들끼리만 비교 할 수도 있겠음. 
+그런데 해시를 사용한다는 조건이 있기 때문에 그대로 두겠음.
+"""
 def solution(phone_book):
     phone_book.sort(key=len)
     phone_set = set()
@@ -14,7 +17,12 @@ def solution(phone_book):
         phone_set.add(phone)
 
     return True
+
 """
+해시를 생각하지 않는다면 아래와 같이 풀 수 있음. 
+정렬 후 인접한 것들끼리 비교. 
+그냥 sort하면 문자열 순으로 정렬됨.
+
 def solution(phoneBook):
     #phoneBook = sorted(phoneBook)
     phoneBook.sort()
@@ -26,6 +34,7 @@ def solution(phoneBook):
         if p2.startswith(p1):
             return False
     return True
+"""
 
 
 phone_book = ["119","97674223","1195524421"]
