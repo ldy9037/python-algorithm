663f341470de7bf47747f5fd81fa02c65d947daf
emadam <ldy9037@naver.com>
2022-04-29 22:21:26 +0900

    다른 풀이 추가 및 코멘트

--- algorithm-lv2/q10.py
+++ algorithm-lv2/q10.py
@@ -29,3 +29,15 @@ def solution(numbers):
 
 numbers = "1231" # 2
 print(solution(numbers))
+
+"""
+에라스토테네스 체를 사용한 풀이. 
+    def solution(n):
+    a = set()
+    for i in range(len(n)):
+        a |= set(map(int, map("".join, permutations(list(n), i + 1))))
+    a -= set(range(0, 2))
+    for i in range(2, int(max(a) ** 0.5) + 1):
+        a -= set(range(i * 2, max(a) + 1, i))
+    return len(a)
+"""
