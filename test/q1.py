05a40646159e260a198cc19cc1bd6aec0f168dfe
emadam <ldy9037@naver.com>
2022-05-06 18:28:57 +0900

    카카오 인턴쉽 - 숫자 문자열과 영단어

---
+++ test/q1.py
@@ -0,0 +1,6 @@
+# 카카오 인턴쉽 - 숫자 문자열과 영단어
+def solution(s):
+    en_num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    
+    for en in en_num: s = s.replace(en, str(en_num.index(en)))
+    return int(s)
