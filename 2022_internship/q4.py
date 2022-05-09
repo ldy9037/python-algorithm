3fe60918eb6c95aabe39de1d66e4c71ddf7565d7
emadam <ldy9037@naver.com>
2022-05-09 13:06:58 +0900

    코드 변수 구성 정리 - gates를 copy해서 remove하던 작업을 deque로 pop, push하도록 변경 - 변수 선언 순서 변경

--- 2022_internship/q4.py
+++ 2022_internship/q4.py
@@ -1,55 +1,56 @@
 # 카카오 인턴십 - 4번
 
 import heapq
-
+from collections import deque 
 
 def solution(n, paths, gates, summits):
     answer = []
     last_maxium = []
 
-    left = 1
-    right = 10000000
+    left, right = 1, 10000000
     
+    gates = deque(gates)
     graph = [[] for _ in range(n)]
 
     for s, d, time in paths:
         graph[s - 1].append((d - 1, time))
         graph[d - 1].append((s - 1, time))
-    
+
     while left <= right:
         middle = (left + right) // 2          
-
         maxium = []
-
-        for gate in gates: 
-            other_gates = gates.copy()
-            other_gates.remove(gate)
-            gate -= 1
+        
+        for i in range(len(gates)):
             times = [float("inf")] * n
             visited = [False]* n
+            
+            gate = gates.popleft() - 1
+            
             times[gate] = 0
+            visited[gate] = True 
 
             heap = []
             heapq.heappush(heap, (0, gate, []))
-            
+    
             while heap:
                 time, current, history = heapq.heappop(heap)
-                visited[current] = True
+                
                 if (current + 1) in summits: 
-                    
                     maxium.append((current + 1,max(history)))
-
-
+                    if left != right: break
                 if times[current] < time: continue
+
                 for destination, next_time in graph[current]: 
-                    
-                    if next_time > middle or (destination + 1) in other_gates or visited[current]: continue
+                    if next_time > middle or (destination + 1) in gates or visited[destination]: continue
                     total_time = time + next_time
-                    
+
                     if total_time < times[destination]: 
                         history = history + [next_time]
                         times[destination] = total_time
+                        
                         heapq.heappush(heap, (total_time, destination, history))
+
+            gates.append(gate + 1)
         
         if len(maxium) > 0: 
             last_maxium = maxium
