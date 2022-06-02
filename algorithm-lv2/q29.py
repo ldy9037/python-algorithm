22fb5b2cdc600c3a9f56b695c80e0b9992de3bc3
emadam <ldy9037@naver.com>
2022-06-02 13:33:00 +0900

    월간 코드 챌린지 - 빛의 경로 사이클

---
+++ algorithm-lv2/q29.py
@@ -0,0 +1,53 @@
+# 월간 코드 챌린지 - 빛의 경로 사이클
+def move(arrow, x, y, limit):
+    row_limit, col_limit = limit
+    if arrow == "e": y = y + 1 if y < col_limit - 1 else 0
+    if arrow == "w": y = y - 1 if y > 0 else col_limit - 1
+    if arrow == "n": x = x - 1 if x > 0 else row_limit - 1
+    if arrow == "s": x = x + 1 if x < row_limit - 1 else 0
+
+    return (x, y)
+
+def start_location(visited):
+    for i in range(len(visited)):
+        for k in range(len(visited[i])):
+            if not all(visited[i][k]): return (visited[i][k].index(False), i, k)
+    
+    return False
+
+def solution(grid):
+    answer = []
+    arrow = ["n", "e", "s", "w"]
+    
+    visited = []
+    for i in range(len(grid)):
+        visited.append([])
+        for k in range(len(grid[0])):
+            visited[i].append([False] * 4)
+
+    while True:
+        check_result = start_location(visited)
+        if not check_result: break 
+        
+        history = [check_result] # arrow, x, y 0,0,0이면 동쪽으로 가서 x,y에 도착했다는 뜻
+        
+        while len(history) <= 1 or history[0] != history[-1]:
+            
+
+            p_arrow, row, col = history[-1]        
+            
+            arrow_index = p_arrow
+            if grid[row][col] == "L": arrow_index = p_arrow - 1 if p_arrow > 0 else 3
+            if grid[row][col] == "R": arrow_index = p_arrow + 1 if p_arrow < 3 else 0
+            
+            n_row, n_col = move(arrow[arrow_index], row, col, (len(grid), len(grid[0]))) 
+
+            history.append((arrow_index, n_row, n_col))
+            visited[n_row][n_col][arrow_index] = True 
+        
+        answer.append(len(history) - 1)
+
+    return sorted(answer)
+
+grid = ["R","R"]	
+print(solution(grid))
