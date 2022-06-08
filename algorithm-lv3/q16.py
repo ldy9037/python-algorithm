263d0ef418e5f5f1d9a79234dd431e4aa2635f89
emadam <ldy9037@naver.com>
2022-06-08 12:25:40 +0900

    예외처리를 통해 성능 개선

--- algorithm-lv3/q16.py
+++ algorithm-lv3/q16.py
@@ -1,4 +1,7 @@
 # Dev Matching - 다단계 칫솔 판매
+# 문제 자체는 굉장히 쉬우나 범위를 생각하지 않고 예외처리를 하지 않으면 고생 할 수 있는 문제
+# amount 의 범위가 100 이하이기 때문에 위로 올라가는 수수료는 최대 4번 까지만 올라감. 10000 -> 1000 -> 100 -> 10 -> 1
+
 """
 def solution(enroll, referral, seller, amount):
     answer = []
@@ -27,26 +30,25 @@ def solution(enroll, referral, seller, amount):
 def solution(enroll, referral, seller, amount):
     answer = [0] * len(enroll)
 
+    c_index = dict(zip(enroll, range(len(enroll))))
+
     stack = []
     for s, a in zip(seller, amount):
         stack.append((s, a * 100))
-
-    print(stack)
 
     while stack:
         s, a = stack.pop()
         
         if s == "-": continue
         
-        c_index = enroll.index(s)
-        
-        next = referral[c_index]
+        next = referral[c_index[s]]
         r = a // 10
 
-        answer[c_index] += a - r
+        answer[c_index[s]] += a - r
 
-        a = r
-        stack.append((next, a))
+        if r > 0:
+            a = r
+            stack.append((next, a))
 
     return answer
 
