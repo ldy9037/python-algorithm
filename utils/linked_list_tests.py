cab85c55a76946a37e14bb64ae3debd224e7e35d
emadam <ldy9037@naver.com>
2022-06-16 12:44:17 +0900

    List의 상태 유지를 위해 then을 setup에서 수행하도록 변경

--- utils/linked_list_tests.py
+++ utils/linked_list_tests.py
@@ -4,21 +4,19 @@ from linked_list import Node
 
 class TestLinkedList(unittest.TestCase):
 
-    l_list = None
+    l_list = LinkedList()
 
     def setUp(self):
-        self.l_list = LinkedList()
+        print("- Then: 노드가 5새 세팅되어 있을 때")
 
-    def test_then(self):
-        print("- 노도가 5개 세팅 되어 있을 때")
-        
         for i in range(5):
             self.l_list.append(Node(i, None, None))
 
-        self.assertEqual(self.l_list.length(), 5, "노드가 정상적으로 세팅되지 않았습니다.")
-
     def tearDown(self):
         print('[현재 Linked List 내부] : ', self.l_list.toList())
+
+    def test_when(self):    
+        self.assertEqual(self.l_list.length(), 5, "노드가 정상적으로 세팅되지 않았습니다.")
         
 if __name__ == '__main__':
     unittest.main()
