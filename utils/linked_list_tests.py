d62c7575f301b18ee9cc33d2913025380dcab043
emadam <ldy9037@naver.com>
2022-06-16 12:48:54 +0900

    When에 삽입에 대한 행위 추가

--- utils/linked_list_tests.py
+++ utils/linked_list_tests.py
@@ -17,6 +17,11 @@ class TestLinkedList(unittest.TestCase):
 
     def test_when(self):    
         self.assertEqual(self.l_list.length(), 5, "노드가 정상적으로 세팅되지 않았습니다.")
+
+        print("- When: 3번째 위치(index = 2)에 값이 0인 노드를 삽입하고")
+        self.l_list.add(2, Node(0))
+        self.assertEqual(self.l_list.length(), 6, "노드가 정상적으로 삽입되자 않았습니다.")
+        self.assertEqual(self.l_list.findNodeByIndex(2).getValue(), 0, "노드가 잘못된 위치에 삽입되었습니다.")
         
 if __name__ == '__main__':
     unittest.main()
