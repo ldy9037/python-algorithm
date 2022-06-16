eb2ce9e70064826220fd86e633e2365540c861b8
emadam <ldy9037@naver.com>
2022-06-16 12:23:13 +0900

    LinkedList Class에 대한 테스트 파일 생성 및 초기 셋업

---
+++ utils/linked_list_tests.py
@@ -0,0 +1,21 @@
+import unittest
+from linked_list import LinkedList
+from linked_list import Node
+
+class TestLinkedList(unittest.TestCase):
+
+    l_list = None
+
+    def setUp(self):
+        self.l_list = LinkedList()
+
+    def test_then(self):
+        print("노도가 5개 세팅 되어 있을 때")
+        
+        for i in range(5):
+            self.l_list.append(Node(i, None, None))
+
+        self.assertEqual(self.l_list.length(), 5, "노드가 정상적으로 세팅되지 않았습니다.")
+
+if __name__ == '__main__':
+    unittest.main()
