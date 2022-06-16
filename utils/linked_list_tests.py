88d076713b261b5eda7116185dc249f9eed0e3a0
emadam <ldy9037@naver.com>
2022-06-16 12:31:07 +0900

    tearDown 메서드로 매 테스트 마다 현재 List출력

--- utils/linked_list_tests.py
+++ utils/linked_list_tests.py
@@ -10,12 +10,15 @@ class TestLinkedList(unittest.TestCase):
         self.l_list = LinkedList()
 
     def test_then(self):
-        print("노도가 5개 세팅 되어 있을 때")
+        print("- 노도가 5개 세팅 되어 있을 때")
         
         for i in range(5):
             self.l_list.append(Node(i, None, None))
 
         self.assertEqual(self.l_list.length(), 5, "노드가 정상적으로 세팅되지 않았습니다.")
 
+    def tearDown(self):
+        print('[현재 Linked List 내부] : ', self.l_list.toList())
+        
 if __name__ == '__main__':
     unittest.main()
