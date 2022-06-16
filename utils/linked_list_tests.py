f9bbe94dfd9f507eea6174f86dc7fcfab2278e6b
emadam <ldy9037@naver.com>
2022-06-17 08:04:55 +0900

    list 상태 유지를 위해 setUp 제거

--- utils/linked_list_tests.py
+++ utils/linked_list_tests.py
@@ -6,23 +6,22 @@ class TestLinkedList(unittest.TestCase):
 
     l_list = LinkedList()
 
-    def setUp(self):
-        print("- Then: 노드가 5새 세팅되어 있을 때")
-
-        for i in range(5):
-            self.l_list.append(Node(i, None, None))
-
     def tearDown(self):
         print('[현재 Linked List 내부] : ', self.l_list.toList())
 
-    def test_when(self):    
+    def test_then(self):
+        print("- Then: 값이 0 ~ 5인 노드 5개가 세팅되어 있을 때")
+        for i in range(5):
+            self.l_list.append(Node(i, None, None))
+
         self.assertEqual(self.l_list.length(), 5, "노드가 정상적으로 세팅되지 않았습니다.")
 
+    def test_when(self):    
         print("- When: 3번째 위치(index = 2)에 값이 0인 노드를 삽입하고")
         self.l_list.add(2, Node(0))
         self.assertEqual(self.l_list.length(), 6, "노드가 정상적으로 삽입되자 않았습니다.")
-        self.assertEqual(self.l_list.findNodeByIndex(2).getValue(), 0, "노드가 잘못된 위치에 삽입되었습니다.")
-        
+        self.assertEqual(self.l_list.findNodeByIndex(2).getValue(), 0, "노드가 잘못된 위치에 삽입되었습니다. ")
+
         print("- When: 값이 3인 노드 뒤에 값이 7인 노드를 삽입한 뒤")
         self.l_list.addAfterValue(3, Node(7))
         self.assertEqual(self.l_list.length(), 7, "노드가 정상적으로 삽입되자 않았습니다.")
