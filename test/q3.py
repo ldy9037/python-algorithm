9d3c0f619881ed33c27f71d93d45f57e87698eff
emadam <ldy9037@naver.com>
2022-06-18 22:07:36 +0900

    Linked List 적용해서 성능 개선 - 효율성 일부 통과 못함

--- test/q3.py
+++ test/q3.py
@@ -1,4 +1,5 @@
 # 카카오 인턴쉽 - 표 편집
+
 """
 "U X": 현재 선택된 행에서 X칸 위에 있는 행을 선택합니다.
 "D X": 현재 선택된 행에서 X칸 아래에 있는 행을 선택합니다.
@@ -7,113 +8,107 @@
 """
 
 """
-linked_list 시작
+linked_list by ldy9037@naver.com
 """
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
 
     def setHead(self, node):
         self.head = node
 
+    def setTail(self, node):
+        self.tail = node
+
     def getHead(self):
         return self.head
+
+    def getTail(self):
+        return self.tail
 
     def length(self):
         result = 0
         current = self.getHead()
 
         while current != None:
-            current = current.getLink()
+            current = current.getNext()
             result += 1
 
         return result
 
-    def add(self, index, node):             
+    def connectNode(self, prev, current, next):
+        current.setPrev(prev)
+        current.setNext(next)
+
+        if current.getNext() == None: self.setTail(current)
+        else: current.getNext().setPrev(current)
+
+        if current.getPrev() == None: self.setHead(current)
+        else: current.getPrev().setNext(current)
+
+    def disconnectNode(self, prev, next):
+        if prev == None: self.setHead(next)
+        else: prev.setNext(next)
+
+        if next == None: self.setTail(prev)
+        else: next.setPrev(prev)
+
+    def findNodeByIndex(self, index):
         current = {"index": 0, "node": self.getHead()}
-        prev = None
-
-        while current["index"] <= index:
-            if current["index"] == index:
-                node.setLink(current["node"])
-
-                if prev == None: self.setHead(node)
-                else: prev.setLink(node)
-                break
-
-            prev = current["node"]
-            current["node"] = current["node"].getLink()                
-            current["index"] += 1
         
-        if current["index"] > index: return -1
+        while current["node"] != None and current["index"] < index:
+            current["index"] += 1
+            current["node"] = current["node"].getNext()
+
+        return current["node"]        
+
+    def findNodeByValue(self, value):
+        current = self.getHead()
+
+        while current != None and current.getValue() != value:
+            current = current.getNext()
+
+        return current
+
+    def add(self, index, node):             
+        current = self.findNodeByIndex(index)
+        if current == None: return False
+        
+        self.connectNode(current.getPrev(), node, current)
+
+        return True
 
     def addAfterValue(self, value, node):
-        current = self.getHead()
+        current = self.findNodeByValue(value)
+        if current == None: return False
         
-        while current != None: 
-            if current.getValue() == value: 
-                node.setLink(current.getLink())
-                current.setLink(node)
-                break
-            
-            current = current.getLink()
-        
-        return -1
+        self.connectNode(current, node, current.getNext())
+
+        return True
 
     def append(self, node):
-        current = self.getHead()
-
-        if current == None: 
-            self.setHead(node)
-            return
-
-        while current != None:
-            if current.getLink() == None: 
-                current.setLink(node)
-                break
-            
-            current = current.getLink()
+        self.connectNode(self.getTail(), node, None)
 
     def remove(self, index): 
-        current = {"index": 0, "node": self.getHead()}
-        prev = None
-
-        while current["index"] <= index:
-            if current["index"] == index:
-                if prev == None: self.setHead(current["node"].getLink())
-                else: prev.setLink(current["node"].getLink())
-                return current["node"].getValue()
-            
-            prev = current["node"]
-            current["node"] = current["node"].getLink()
-            current["index"] += 1
+        current = self.findNodeByIndex(index)
+        if current != None: self.disconnectNode(current.getPrev(), current.getNext())
         
-        return None
+        return current
 
     def removeByValue(self, value):
-        current = {"index": 0, "node": self.getHead()}
-        prev = None
+        current = self.findNodeByValue(value)
+        if current != None: self.disconnectNode(current.getPrev(), current.getNext())
 
-        while current["node"] != None:
-            if current["node"].getValue() == value:
-                prev.setLink(current["node"].getLink())
-                return current["index"]
-
-            prev = current["node"]
-            current["node"] = current["node"].getLink() 
-            
-        return -1
+        return current
 
     def getValue(self, index):
-        current = {"index": 0, "node": self.getHead()}
-        
-        while current["node"] != None: 
-            if current["index"] == index: return current["node"].getValue()
+        result = None
 
-            current["node"] = current["node"].getLink()
-            current["index"] += 1
+        current = self.findNodeByIndex(index)
+        if current != None: result = current.getValue()
         
-        return None
+        return result
 
     def toList(self):
         result = []
@@ -122,64 +117,73 @@ class LinkedList:
         while current != None: 
             result.append(current.getValue())
             
-            current = current.getLink()
+            current = current.getNext()
         
         return result
 
 class Node:
-    def __init__(self, value, link=None):
+    def __init__(self, value, prev=None, next=None):
         self.value = value
-        self.link = link
-
-    def setLink(self, node):
-        self.link = node
+        self.prev = prev
+        self.next = next
     
+    def setNext(self, next):
+        self.next = next
+
+    def setPrev(self, prev):
+        self.prev = prev
+
     def setValue(self, value):
         self.value = value
     
-    def getLink(self):
-        return self.link 
-    
+    def getNext(self):
+        return self.next 
+
+    def getPrev(self):
+        return self.prev 
+
     def getValue(self):
         return self.value
+
 """
-linked_list 끝
+linked list 끝
 """
+
 def solution(n, k, cmd_list):
-    answer = 'O' * n
+    answer = ['O'] * n
     
     delete_stack = []
-    selected = k    
     table = LinkedList()
 
-    for i in range(n): table.append(Node(i))
+    for i in range(n): table.append(Node(i, None, None))
+    selected = table.findNodeByIndex(k)
 
     for cmd in cmd_list:
         if len(cmd) >= 2:
             direction, count = cmd.split()
             count = int(count)
-            selected = selected - count if direction == "U" else selected + count
+
+            for i in range(count):
+                selected = selected.getPrev() if direction == "U" else selected.getNext()
         if  len(cmd) == 1:
             if cmd == "C": 
-                delete_stack.append((selected, table.remove(selected)))
-                if selected >= table.length(): selected -= 1
+                table.disconnectNode(selected.getPrev(), selected.getNext())
+                delete_stack.append(selected)
+
+                selected = selected.getNext() if selected.getNext() != None else selected.getPrev()
             else: 
-                index, value = delete_stack.pop()  
-
-                if table.length() <= index: table.append(Node(value))
-                else: table.add(index, Node(value))
-
-                if index <= selected: selected += 1
+                node = delete_stack.pop()
+                table.connectNode(node.getPrev(), node, node.getNext())
 
     while delete_stack:
-        index, value = delete_stack.pop()
-        answer = answer[:value]+'X'+answer[value + 1:]
+        node = delete_stack.pop()
+        answer[node.getValue()] = 'X'
     
-    return answer
+    return ''.join(answer)
 
 n = 8
 k = 2
-#cmd = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]
-cmd = ["U 2", "C","Z", "C","Z", "C","Z"]
+cmd = ["D 2","C","U 3","C","D 4","C","U 2","Z","Z","U 1","C"]
+#cmd = ["U 2", "C","Z", "C","Z", "C","Z"]
 #OOXOXOOO
 print(solution(n, k, cmd))
