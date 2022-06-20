f96b40f8d97b7b63a2d44d99ac21bb081ec00605
emadam <ldy9037@naver.com>
2022-06-20 21:40:44 +0900

    dict를 사용해서 성능 개선

--- algorithm-lv3/q19.py
+++ algorithm-lv3/q19.py
@@ -1,36 +1,50 @@
 # 카카오 인턴십 - 보석쇼핑
-
-"""
 from collections import deque
 
 def solution(gems):
     answer = []
 
-    current = [1, 1]
-    kinds_count = len(set(gems))
-    bucket = deque()
-    length = 0
+    current = [1, 0]
 
-    for gem in gems: 
-        if not gem in bucket: length += 1
-        bucket.append(gem)
+    gem_kind = set(gems)
+    kind_cnt = dict(zip(gem_kind, [0] * len(gem_kind)))
 
-        if len(bucket) > 1:
-            e = bucket.popleft()
+    order = deque()
+    complete = False
 
-            while length == len(set(bucket)): 
-                current[0] += 1 
-                e = bucket.popleft()
+    for gem in gems:
+        order.append(gem)
+        kind_cnt[gem] += 1
 
-            bucket.appendleft(e)
+        while kind_cnt[order[0]] > 1:
+            f = order.popleft()
+            kind_cnt[f] -= 1
 
-        if length == kinds_count: 
-            answer.append(current.copy())
+            if kind_cnt[f] == 0: 
+                order.appendleft(f)
+                kind_cnt[f] += 1
+            else: current[0] += 1
+
         current[1] += 1
+        
+        if complete == False:
+            complete = True
 
-    answer.sort(key=lambda x: x[1] - x[0])
+            for v in kind_cnt.values():
+                if v == 0: 
+                    complete = False
+                    break
     
+        if complete: answer.append(current.copy())
+            
+    print(answer)
+    answer.sort(key=lambda x: x[1] - x[0])
     return answer[0]
+
+gems = 	["DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA"]
+
+print(solution(gems))
+# 3,7
 """
 from collections import deque
 
@@ -71,7 +85,4 @@ def solution(gems):
     answer = check(gems, left, kind_count)
 
     return answer
-
-gems = ["ZZZ", "YYY", "NNNN", "YYY", "BBB"]
-
-print(solution(gems))
+"""
