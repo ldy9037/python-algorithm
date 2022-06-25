54a03988d3797fa1ba65d14cfbffb881de2da6ff
emadam <ldy9037@naver.com>
2022-06-25 13:51:12 +0900

    주석 제거

--- algorithm-lv3/q21.py
+++ algorithm-lv3/q21.py
@@ -1,47 +1,4 @@
 # 월간 코드 챌린지 - 금과 은 운반하기
-"""
-def solution(a, b, g, s, w, t):
-    answer = 0
-    
-    left = 0
-    right = (10 ** 9) * 2
-
-    while left < right:
-        middle = (left + right) // 2
-
-        success = False
-        t_a, t_b = 0, 0
-        for i in range(len(t)):
-            time = 0
-            gold, silver = g[i], s[i]
-            while time < middle: 
-                weight = w[i]
-
-                g_w = weight if gold - weight > 0 else gold
-                g_w = g_w if t_a + g_w < a else a - t_a
-                gold -= g_w
-
-                s_w = weight - g_w if silver - (weight - g_w) > 0 else silver
-                s_w = s_w if t_b + s_w < b else b - t_b
-                silver = silver - s_w
-
-                t_a = g_w + t_a
-                t_b = s_w + t_b
-            
-                if g_w == 0 and s_w == 0: break
-                time = time + t[i] if time == 0 else time + (t[i] * 2)
-
-            if t_a == a and t_b == b and time <= middle: 
-                success = True
-                break
-
-        if success: 
-            right = middle
-            answer = middle
-        else: left = middle + 1
-        
-    return answer
-"""
 import math
 
 def solution(a, b, g, s, w, t):
