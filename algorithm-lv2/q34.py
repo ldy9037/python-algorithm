eb756dca67d90f11e39be2d5a6738ea42165efc0
emadam <ldy9037@naver.com>
2022-06-25 21:22:35 +0900

    완벽한 괄호가 되지 않을 경우 0 을 리턴하도록 수정

--- algorithm-lv2/q34.py
+++ algorithm-lv2/q34.py
@@ -1,4 +1,5 @@
 # 월간 코드 챌린지 - 괄호 회전하기
+# 괄호를 회전시키면서 완벽환 괄호가 되는지 확인하고 완벽한 괄호가 될 경우 부분괄호의 개수(answer)를 구함
 from collections import deque
 
 def solution(s):
@@ -10,7 +11,6 @@ def solution(s):
     for i in range(len(s)):
         if sorted: break
         
-        index = 0
         sub_group = []
         stack = []
 
@@ -22,17 +22,22 @@ def solution(s):
                 if check == 1 or check == 2 : stack.pop()
                 else: stack.append(bracket)
             else:
-                if k != 0: sub_group.append(index)
+                if k != 0: sub_group.append(i)
                 stack.append(bracket)
-
-            index += 1
 
         if not stack: 
             sorted = True
             sub_group.append(k)
         else: queue.append(queue.popleft())
 
-    return len(sub_group)
+        answer = len(sub_group) if sorted else 0
 
-s = "({{}}())"
+    return answer
+
+s ="))("
 print(solution(s))
+"""
+()(
+(
+{{{{{{
+"""
