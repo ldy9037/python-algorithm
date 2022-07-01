f4f6ffcee463f8d5b2245fceb6caf5284eb4d18f
emadam <ldy9037@naver.com>
2022-07-01 20:32:36 +0900

    refactor: DP 점화식을 변경해서 성능 개선

--- algorithm-lv4/q5.py
+++ algorithm-lv4/q5.py
@@ -22,7 +22,7 @@ def solution(strs, t):
 
     answer = min(result) if result else -1
     return answer
-"""
+
 from collections import defaultdict
 from collections import deque
 
@@ -34,30 +34,60 @@ def solution(strs, t):
 
     t = deque(t)
 
+    minimum = float("inf")
     string = ""
     while t:
         string += t.popleft()
-        
         
         if len(string) <= 5 and string in min_dict: 
             min_dict[string] = 1
             continue
         
-        for i in range(6):
-            f, b = string[:len(string) - i], string[len(string) - i:]
+        b = ""
+        for i in range(5):
+            if string == "": break
+            b = string.pop() + b
 
-            if min_dict[f] == 0 or min_dict[b] == 0: continue
+            if min_dict[string] == 0 or min_dict[b] == 0: continue
 
-            cnt = min_dict[f] + min_dict[b] 
+            cnt = min_dict[string] + min_dict[b] 
+            if minimum == float("inf"): minimum = cnt
             if min_dict[string] == 0 or cnt < min_dict[string] :
                 min_dict[string] = cnt 
-            if cnt <= 2: break
+            if cnt == 1 and cnt <= minimum: break
 
+        string += b
         if not string in min_dict: min_dict[string] = 0
 
     answer = min_dict[string] if min_dict[string] != 0 else -1
             
     return answer
+"""
+from collections import deque
+
+def solution(strs, t):
+    answer = [float("inf")] * len(t)   
+
+    string = ""
+    for i in range(len(t)):
+        string += t[i]
+     
+        for k in range(1,6):
+            b = string[-k:]
+            
+            if b in strs:
+                if b == string: 
+                    answer[i] = 1
+                    break
+            
+                answer[i] = min(answer[i], answer[i - (len(b))] + 1)
+            
+            if b == string: break
+    
+    answer = answer[-1] if answer[-1] != float("inf") else -1
+
+    return answer
+
 
 
 strs = 	["ab", "na", "n", "a", "bn"]
