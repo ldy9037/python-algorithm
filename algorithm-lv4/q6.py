3087ea2ee01c7293fd68f16710230a1d8abd5ace
emadam <ldy9037@naver.com>
2022-07-12 19:14:55 +0900

    trie 사용해서 정확성 통과

--- algorithm-lv4/q6.py
+++ algorithm-lv4/q6.py
@@ -4,28 +4,36 @@ from collections import defaultdict
 def solution(words, queries):
     answer = [0] * len(queries) 
 
-    trie = defaultdict(int)
-    trie_reverse = defaultdict(int)
+    trie = dict()
+    trie_reverse = dict()
     for word in words:
-        for i in range(1, len(word) + 1):
-            trie[word[:i]] += 1
-            trie_reverse[word[-i:]] += 1
+        word_length = len(word)
+        if not word_length in trie:
+            trie[word_length] = defaultdict(int)
+            trie_reverse[word_length] = defaultdict(int)
 
-    print(trie_reverse)
+        trie_reverse[word_length][""] += 1
+
+        for i in range(1, word_length + 1):
+            trie[word_length][word[:i]] += 1
+            trie_reverse[word_length][word[-i:]] += 1
 
     for i in range(len(queries)):
+
+        query_length = len(queries[i])
+        if not query_length in trie: continue
         query = queries[i]
         front = (query[0] == "?")
         
         query = query.replace("?", "")
-        
-        if front: answer[i] = trie_reverse[query]
-        else: answer[i] = trie[query]
 
+        if front: answer[i] = trie_reverse[query_length][query]
+        else: answer[i] = trie[query_length][query]
+ 
     return answer
 
 words = ["frodo", "front", "frost", "frozen", "frame", "kakao"]	
-queries = ["fro??", "????o", "fr???", "fro???", "pro?"]	
+queries = ["fro??", "????o", "fr???", "??????", "pro?"]	
 print(solution(words, queries))
 
 # [3, 2, 4, 1, 0]
