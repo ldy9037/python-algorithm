e0e5744812b56646b5e322e442b566349cead439
emadam <ldy9037@naver.com>
2022-07-19 17:56:45 +0900

    docs: 드 설명 추가

--- dev-matching/test2.py
+++ dev-matching/test2.py
@@ -1,49 +1,75 @@
 from collections import deque
 def solution(n, horizontal):
+    
     answer = [] * n
+    # 배열 주소 참조 방지하기 위해 loop로 2차원 배열 생성
     for i in range(n): answer.append([0] * n)
 
+    # 시작은 [0][0]으로 고정
     answer[0][0] = 1
 
+    # BFS를 적용하기 위해 queue 생성
     queue = deque()
+
+    # horizontal이라면 오른쪽으로 시작 아니라면 아래로 시작
     if horizontal:queue.append((0, 1, True))
     else: queue.append((1, 0, True))    
     
+    # BFS에서 현재 순서를 체크하기 위해 order 변수 생성
     order = 1
     while queue:
         order += 1
+        # 큐에서 BFS 다음 정점을 꺼냄(방문), 정점 정보는 (row, col, 끝에 있는 정점인지)
         x, y, end = queue.popleft()
 
+        # 만약 좌표가 배열을 벗어났다면 중지
         if x >= n or y >= n: break
         
+        # 현재 좌표에 순번 지정
         answer[x][y] = order
         
+        # 만약 현재 정점이 끝에 있는 정점이라면 (방향전환 하는 부분이라면)
         if end:
+            # 방향 전환 후 시작 좌표 초기화
             n_x = 0
             n_y = 0
 
-            if x == 0:              
-                n_y = y
+            # 현재 정점이 위에 존재한다면(다음 방향은 아래)
+            if x == 0:  
+
+                # 다음 시작 좌표의 col은 현재 col (아래로 이동하므로)            
+                n_y = y 
+
+                # 다음 끝점까지는 현재 col의 두배 만큼 이동해야함
                 for i in range(y * 2):
+                    # row를 한칸 씩 늘리면서 방문 예약(Enqueue)
                     if n_x < y: 
                         n_x += 1
                         queue.append((n_x, y, False))
+                    # 한칸씩 늘리다가 row와 col이 같아지는 시점에서부터 col을 한칸씩 줄여가면서 방문 예약
                     else: 
                         n_y -= 1
                         queue.append((n_x ,n_y, False))
-                
+                # 해당 라인을 전부 방문 예약 했으므로 다음 라인으로 이동(방문) 예약 (아래로 이동/ 방향전환 준비)
                 queue.append((n_x + 1 ,n_y, True))
                         
+            # 현재 정점이 왼쪽에 존재한다면 (다음 방향을 오른쪽)
             if y == 0: 
+
+                # 다음 시작 좌표의 row는 현재 row (오른쪽으로 이동하므로)            
                 n_x = x
+
+                # 마찬가지로 다음 끝점까지 이동횟수는 현재 row의 두배
                 for i in range(x * 2): 
+                    # col을 한칸 씩 늘리면서 방문 예약(Enqueue)
                     if n_y < x:
                         n_y += 1
                         queue.append((x, n_y, False))
+                    # 한칸씩 늘리다가 row와 col이 같아지는 시점에서부터 row를 한칸씩 줄여가면서 방문 예약
                     else:
                         n_x -= 1
                         queue.append((n_x, n_y, False))
-                
+                # 해당 라인을 전부 방문 예약 했으므로 다음 라인으로 이동(방문) 예약 (오른쪽 이동/ 방향전환 준비)
                 queue.append((n_x ,n_y + 1, True))
         
     return answer
--- dev-matching/test3.py
+++ dev-matching/test3.py
@@ -1,5 +1,7 @@
 def check_push(table, stack, visited, x, y):
+    # 해당 지점이 물이고 방문한적이 없다면 방문 예약(push)
     if table[x][y] == 0 and not visited[x][y]: 
+        # 방문 처리 (중복 방문 예약을 방지하기 위해 미리 방문 처리)
         visited[x][y] = True
         stack.append((x, y))
     
@@ -8,38 +10,59 @@ def check_push(table, stack, visited, x, y):
 def solution(rows, columns, lands):
     answer = []
 
+    # DFS로 넓이를 구할 것이기 때문에 중복방문을 피하기 위해 해당 지점을 방문했는지 체크
     visited = [] * (rows)
+    # 배열 주소 참조를 방지하기 위해 loop로 2차원 배열 생성
     for i in range(rows):
         visited.append([False] * (columns))
 
+    # rows * columns 크기의 table을 생성
+    # 배열 주소 참조를 방지하기 위해 loop로 2차원 배열 생성
     table = [] * rows
     for i in range(rows): table.append([0] * columns)
+    # table(지도)상에 땅인 부분 1로 표시
     for x,y in lands: table[x - 1][y - 1] = 1
 
+    # 지도의 처음 부터 탐색 시작 
+    # 지도의 테두리는 바다로 구성되어 있기 때문에 테두리는 제외하고 탐색
     for i in range(rows - 2): 
         for k in range(columns - 2):
+            # 이미 방문한 적이 있는 지점이거나 해당 지점이 땅일 경우는 그냥 다음 지점으로 넘어감
             if visited[i + 1][k + 1] or table[i + 1][k + 1] == 1: continue
 
+            # 위 조건에 해당하지 않는 다면 DFS 시작 (해당 지점이 물이고 첫 방문)
+            # DFS를 위해 stack 생성
             stack = []
+            # 해당 지점부터 시작
             stack.append((i + 1, k + 1))
             
+            # 탐색할 호수 넓이 초기화
             area = 0
+            # 지금 DFS로 탐색하고 있는 영역이 호수인지 여부를 담고 있는 변수 선언
             isLake = True
             while stack: 
+                # 지점 방문 (row, col)
                 x, y = stack.pop()
+                # 방문했음을 표시
                 visited[x][y] = True
+                # 호수 넓이 + 1
                 area += 1
 
+                # 호수가 테두리와 맞닿아 있다면 
                 if x == 0 or y == 0 or x == rows - 1 or y == columns - 1: 
+                    # 이 영역은 호수가 아님 (바다임)
                     isLake = False
                 else: 
+                    # 해당 지점을 기준으로 동서남북 체크 후 방문 예약
                     stack = check_push(table, stack, visited, x + 1, y)
                     stack = check_push(table, stack, visited, x, y + 1)
                     stack = check_push(table, stack, visited, x - 1, y)
                     stack = check_push(table, stack, visited, x, y - 1)
-            
-            if isLake: answer.append(area)    
 
+            # 탐색 결과 이 영역이 호수라면 넓이를 저장            
+            if isLake: answer.append(area)    
+    
+    # 저장된 넓이가 존재하면 최소/최대값 return 만약에 저장된 넓이가 없다면 최소/최대 모두 -1로 return
     return [min(answer), max(answer)] if answer else [-1, -1]
 
 rows = 5
