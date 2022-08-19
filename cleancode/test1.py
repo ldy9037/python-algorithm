38d14f5d0d1b6687da941d73e10285340e972a74
emadam <ldy9037@naver.com>
2022-08-19 20:24:11 +0900

    test: mypy로 타입 검사 후 타입 변경

--- cleancode/test1.py
+++ cleancode/test1.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-def test_function(value1:int, value2:str=None) -> Dict[int, str]:
+def test_function(value1:str, value2:str) -> Dict[str, str]:
     """두 개의 값을 전달 받아 dict으로 반환"""
     return {"v1": value1, "v2": value2}
 
