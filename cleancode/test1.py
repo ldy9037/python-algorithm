0eb7ffd9f2f096ed5a4d76f21389f7e0c653c2fc
emadam <ldy9037@naver.com>
2022-08-19 20:27:23 +0900

    test: Black 사용해서 포매팅

--- cleancode/test1.py
+++ cleancode/test1.py
@@ -1,8 +1,10 @@
 from typing import Dict
 
-def test_function(value1:str, value2:str) -> Dict[str, str]:
+
+def test_function(value1: str, value2: str) -> Dict[str, str]:
     """두 개의 값을 전달 받아 dict으로 반환"""
     return {"v1": value1, "v2": value2}
+
 
 print(test_function.__annotations__)
 print(test_function.__doc__)
