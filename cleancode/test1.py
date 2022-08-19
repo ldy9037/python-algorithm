a8885494366e480841a7ced4bfde5e47e63c54bb
emadam <ldy9037@naver.com>
2022-08-19 20:21:25 +0900

    test: typing 모듈 적용

--- cleancode/test1.py
+++ cleancode/test1.py
@@ -1,4 +1,6 @@
-def test_function(value1:int, value2:str=None) -> dict:
+from typing import Dict
+
+def test_function(value1:int, value2:str=None) -> Dict[int, str]:
     """두 개의 값을 전달 받아 dict으로 반환"""
     return {"v1": value1, "v2": value2}
 
